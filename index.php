ed0d53ea5d190a9f3308e155f773a706815c4446
Barış İÇEN <icenbaris50@gmail.com>
2025-07-10 18:11:42 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
-//When you open the project, we first provide redirection to the login page.
+//Projeyi açtığınızda, önce giriş sayfasına yönlendirme sağlıyoruz.
 session_start(); //We started the session
 
-//if there is Session available we redirect the page.
+//Oturum varsa sayfayı yeniden yönlendiriyoruz.
 if (!(isset($_SESSION["Session"]) && $_SESSION["Session"] == "9876")) {
     header("location:login.php");
-} //If remember me is checked beforehand, we create Session and direct the page.
+} //Beni hatırla önceden kontrol edilirse, Oturum oluşturur ve sayfayı yönlendiririz.
 ?>
 <!DOCTYPE html>
 <html>
@@ -20,28 +20,28 @@ if (!(isset($_SESSION["Session"]) && $_SESSION["Session"] == "9876")) {
         <div class="col-md-6">
             <div class="text-center">
                 <h3>LOGİN PDO MD5</h3>
-                <a class="btn btn-danger" href="logout.php">Sign OUT</a>            
+                <a class="btn btn-danger" href="logout.php">Çıkış Yap</a>            
             </div>
             <br/>
         </div>
         <div class="col-md-6">
          <div class="card">
-            <a class="btn btn-success" href="useradd.php">New User ADD</a> 
+            <a class="btn btn-success" href="useradd.php">Yeni Kullanıcı EKLE</a> 
             <div class="card-body">
                 <div class="table-responsive">
                     <table class="table table-striped table-bordered first">
                         <thead>
                             <th>ID</th>
-                            <th>User name</th>
-                            <th>Edit</th>
-                            <th>Delete</th>
+                            <th>Kullanıcı adı</th>
+                            <th>Düzenle</th>
+                            <th>Sil</th>
                         </thead>
                         <tbody>
                             <?php
                             include('fonc.php');
-            $query = $connect->prepare("SELECT * from users"); // We Wrote Our Query
+            $query = $connect->prepare("SELECT * from users"); // Sorgumuzu Yazdık
             $query->execute();
-            while ($result = $query->fetch()) {   // Returning Our Query with While
+            while ($result = $query->fetch()) {   // Sorgumuzu While ile Döndürüyoruz
                 ?>
                 <tr>
                     <td><?=$result['id']?></td>
@@ -60,20 +60,20 @@ if (!(isset($_SESSION["Session"]) && $_SESSION["Session"] == "9876")) {
                                     <div class="modal-header">
                                         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                             <span aria-hidden="true">×</span></button>
-                                            <h4 class="modal-title" id="myModalLabel">Deletion Of User</h4>
+                                            <h4 class="modal-title" id="myModalLabel">Kullanıcının silinmesi</h4>
                                         </div>
                                         <div class="modal-body">
-                                            <h2 style="color: red; text-align: center">Important Warning !</h2>
+                                            <h2 style="color: red; text-align: center">Önemli Uyarı!</h2>
 
                                             <h4 style="text-align: center">
-                                                Are you sure you want to delete<br><b><?php echo $result["user"] ?><br>
+                                                Silmek istediğinden emin misin?<br><b><?php echo $result["user"] ?><br>
                                                </h4>
                                         </div>
                                         <div class="modal-footer">
                                             <button type="button" class="btn btn-default pull-left" data-dismiss="modal">
-                                                Cancel
+                                                İptal
                                             </button>
-                                            <a href="userdelete.php?page=users&amp;id=<?= $result["id"] ?>" class="btn btn-danger">Delete</a>
+                                            <a href="userdelete.php?page=users&amp;id=<?= $result["id"] ?>" class="btn btn-danger">Sil</a>
                                         </div>
                                     </div>
                                 </div>
